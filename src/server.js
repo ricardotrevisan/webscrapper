4ae47549cde3ffb912211455a9460a00c5e6ecf3
Trevisan <ricardo.trevisan@icloud.com>
2021-07-13 19:35:38 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const server= express();
+server.get('/', async (request, response) => {
+    const browser =  await puppeteer.launch();
+    const page =  await browser.newPage();
+    await page.goto('https://consulta-crf.caixa.gov.br/consultacrf/pages/consultaEmpregador.jsf');
+    
+    const content =  await page.evaluate(()=>{
+        return{
+            title: document.querySelector('span h3').innerHTML,
+            description: document.querySelector('.wrapper p').innerHTML
+        }
+    })
+    
+    console.log('pageContent: ', content);
+    response.send({
+        "content": content.title,
+        "description": content.description
+    })
+
+    await browser.close();
+    
+});
+
+server.listen(3000, () => {
+    console.log('✔ O pai tá on! ->  http://localhost:3000')
+});
